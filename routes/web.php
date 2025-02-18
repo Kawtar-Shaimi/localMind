3a5c1a3f556996736201fe884b3beec188c043f3
Kawtar <Kawtar.shaimi8@gmail.com>
2025-02-18 14:43:09 +0100

    Creation du route

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
 use App\Http\Controllers\AnswerController;
-use App\Http\Controllers\QuestionsController;
+use App\Http\Controllers\QuestionController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/', [QuestionController::class, 'index']);
